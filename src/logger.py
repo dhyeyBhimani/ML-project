5ceacf028460a32771bfaccd1be7feebbf37eb27
dhyeyBhimani <dhyeybhimani001@gmail.com>
2024-09-19 01:42:47 +0530

    eda and training

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,19 @@ import logging
 import os
 from datetime import datetime
 
-LOF_FILE = F"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOF_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
-LOF_FILE_PATH = os.path.join(logs_path,LOF_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-logging.basicConfig(filename=LOF_FILE_PATH, level=logging.INFO,
-                    format='%(asctime)s - %(lineno)d %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
 
 # if __name__ == '__main__':
 #     logging.info('logging started')
